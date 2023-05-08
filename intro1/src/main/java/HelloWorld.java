28c2dd945e868d2d5012bfb307d99b376f16c62f
zhangyunzheng <zyz880615@163.com>
2023-05-08 15:52:27 +0800

    First commit

---
+++ intro1/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author zyz
+ * @version 1.0
+ * @date 2023/5/8 15:46
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
