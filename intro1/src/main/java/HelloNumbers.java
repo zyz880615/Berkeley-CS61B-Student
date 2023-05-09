c5ab5e2c0ebff706a5025b69c290c722e9f23c42
zhangyunzheng <zyz880615@163.com>
2023-05-09 08:46:50 +0800

    1. 第一节课课上程序提交

---
+++ intro1/src/main/java/HelloNumbers.java
@@ -0,0 +1,16 @@
+/**
+ * @author zyz
+ * @version 1.0
+ * @date 2023/5/8 16:27
+ */
+public class HelloNumbers {
+
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.print(x + " ");
+            x = x + 1;
+        }
+    }
+
+}
