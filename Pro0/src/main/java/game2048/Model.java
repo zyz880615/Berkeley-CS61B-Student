6638c5d835fcefd0b9cb4da6328537a56b21d170
zhangyunzheng <zyz880615@163.com>
2023-05-11 10:36:03 +0800

    1. Project0提交

---
+++ Pro0/src/main/java/game2048/Model.java
@@ -0,0 +1,314 @@
+package game2048;
+
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+
+
+/** The state of a game of 2048.
+ *  @author zyz
+ */
+public class Model extends Observable {
+    /** Current contents of the board. */
+    private Board board;
+    /** Current score. */
+    private int score;
+    /** Maximum score so far.  Updated when game ends. */
+    private int maxScore;
+    /** True iff game is ended. */
+    private boolean gameOver;
+
+    /* Coordinate System: column C, row R of the board (where row 0,
+     * column 0 is the lower-left corner of the board) will correspond
+     * to board.tile(c, r).  Be careful! It works like (x, y) coordinates.
+     */
+
+    /** Largest piece value. */
+    public static final int MAX_PIECE = 2048;
+
+    /** A new 2048 game on a board of size SIZE with no pieces
+     *  and score 0. */
+    public Model(int size) {
+        board = new Board(size);
+        score = maxScore = 0;
+        gameOver = false;
+    }
+
+    /** A new 2048 game where RAWVALUES contain the values of the tiles
+     * (0 if null). VALUES is indexed by (row, col) with (0, 0) corresponding
+     * to the bottom-left corner. Used for testing purposes. */
+    public Model(int[][] rawValues, int score, int maxScore, boolean gameOver) {
+        int size = rawValues.length;
+        board = new Board(rawValues, score);
+        this.score = score;
+        this.maxScore = maxScore;
+        this.gameOver = gameOver;
+    }
+
+    /** Return the current Tile at (COL, ROW), where 0 <= ROW < size(),
+     *  0 <= COL < size(). Returns null if there is no tile there.
+     *  Used for testing. Should be deprecated and removed.
+     *  */
+    public Tile tile(int col, int row) {
+        return board.tile(col, row);
+    }
+
+    /** Return the number of squares on one side of the board.
+     *  Used for testing. Should be deprecated and removed. */
+    public int size() {
+        return board.size();
+    }
+
+    /** Return true iff the game is over (there are no moves, or
+     *  there is a tile with value 2048 on the board). */
+    public boolean gameOver() {
+        checkGameOver();
+        if (gameOver) {
+            maxScore = Math.max(score, maxScore);
+        }
+        return gameOver;
+    }
+
+    /** Return the current score. */
+    public int score() {
+        return score;
+    }
+
+    /** Return the current maximum game score (updated at end of game). */
+    public int maxScore() {
+        return maxScore;
+    }
+
+    /** Clear the board to empty and reset the score. */
+    public void clear() {
+        score = 0;
+        gameOver = false;
+        board.clear();
+        setChanged();
+    }
+
+    /** Add TILE to the board. There must be no Tile currently at the
+     *  same position. */
+    public void addTile(Tile tile) {
+        board.addTile(tile);
+        checkGameOver();
+        setChanged();
+    }
+
+    /** Tilt the board toward SIDE. Return true iff this changes the board.
+     *
+     * 1. If two Tile objects are adjacent in the direction of motion and have
+     *    the same value, they are merged into one Tile of twice the original
+     *    value and that new value is added to the score instance variable
+     * 2. A tile that is the result of a merge will not merge again on that
+     *    tilt. So each move, every tile will only ever be part of at most one
+     *    merge (perhaps zero).
+     * 3. When three adjacent tiles in the direction of motion have the same
+     *    value, then the leading two tiles in the direction of motion merge,
+     *    and the trailing tile does not.
+     * */
+    public boolean tilt(Side side) {
+
+        boolean changed;
+        changed = false;
+
+        // TODO: Modify this.board (and perhaps this.score) to account
+        // for the tilt to the Side SIDE. If the board changed, set the
+        // changed local variable to true.
+
+        board.setViewingPerspective(side);
+
+        for (int c = 0; c < board.size(); c++){
+            for (int r = board.size()-1; r >= 0; r--){
+                Tile tile = board.tile(c, r);
+                //获取下一行行号
+                int nextRow = checkNextNull(c,r);
+
+                if (nextRow == -1){
+                    break;
+                }
+
+                //获取下一个位置
+                Tile nextTile = board.tile(c, nextRow);
+
+                //当前位置没有数据或当前位置的数据同下一个位置的数据相同才可以移动
+                if (tile == null || tile.value() == nextTile.value()){
+                    changed = true;
+                    //下面的位置移动到当前位置
+                    if (board.move(c, r, nextTile)) {
+                        score += 2 * nextTile.value();
+                    } else {
+                        r++;
+                    }
+                }
+            }
+        }
+
+        board.setViewingPerspective(Side.NORTH);
+
+        checkGameOver();
+        if (changed) {
+            setChanged();
+        }
+        return changed;
+    }
+
+    /**
+     * 获取下一行行号
+     * @param c 列
+     * @param r 行
+     * @return
+     */
+    private int checkNextNull(int c, int r) {
+
+        for (int pos=r-1; pos>=0; pos--){
+            if (board.tile(c, pos) != null){
+                return pos;
+            }
+        }
+
+        return -1;
+    }
+
+    /** Checks if the game is over and sets the gameOver variable
+     *  appropriately.
+     */
+    private void checkGameOver() {
+        gameOver = checkGameOver(board);
+    }
+
+    /** Determine whether game is over. */
+    private static boolean checkGameOver(Board b) {
+        return maxTileExists(b) || !atLeastOneMoveExists(b);
+    }
+
+    /** Returns true if at least one space on the Board is empty.
+     *  Empty spaces are stored as null.
+     * */
+    public static boolean emptySpaceExists(Board b) {
+        // TODO: Fill in this function.
+        if (b.size() == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < b.size(); i++){
+
+            for (int j = 0; j < b.size(); j++){
+
+                if (b.tile(i,j) == null){
+                    return true;
+                }
+
+                if (b.tile(i,j).value()== 0 || b.tile(i,j).value() == 0){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns true if any tile is equal to the maximum valid value.
+     * Maximum valid value is given by MAX_PIECE. Note that
+     * given a Tile object t, we get its value with t.value().
+     */
+    public static boolean maxTileExists(Board b) {
+        // TODO: Fill in this function.
+
+        for (int i = 0; i < b.size(); i++){
+
+            for (int j = 0; j < b.size(); j++){
+
+                if (b.tile(i,j) == null){
+                    continue;
+                }
+
+                if (b.tile(i,j).value() == MAX_PIECE || b.tile(i,j).value() == MAX_PIECE){
+                    return true;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns true if there are any valid moves on the board.
+     * There are two ways that there can be valid moves:
+     * 1. There is at least one empty space on the board.
+     * 2. There are two adjacent tiles with the same value.
+     */
+    public static boolean atLeastOneMoveExists(Board b) {
+        // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+
+            for (int j = 0; j < b.size(); j++){
+
+                if (b.tile(i,j) == null){
+                    return true;
+                }
+
+                if ( i+1 < b.size()  && b.tile(i+1,j) !=null && b.tile(i,j).value() == b.tile(i+1,j).value()){
+                    return true;
+                }
+
+                if ( j+1 < b.size() && b.tile(i,j+1)  != null && b.tile(i,j).value() == b.tile(i,j+1).value()){
+                    return true;
+                }
+
+                if ( i-1 > 0 && b.tile(i-1,j) != null && b.tile(i,j).value() == b.tile(i-1,j).value()){
+                    return true;
+                }
+
+                if ( j-1 > 0 && b.tile(i,j-1) != null && b.tile(i,j).value() == b.tile(i,j-1).value()){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+     /** Returns the model as a string, used for debugging. */
+    public String toString() {
+        Formatter out = new Formatter();
+        out.format("%n[%n");
+        for (int row = size() - 1; row >= 0; row -= 1) {
+            for (int col = 0; col < size(); col += 1) {
+                if (tile(col, row) == null) {
+                    out.format("|    ");
+                } else {
+                    out.format("|%4d", tile(col, row).value());
+                }
+            }
+            out.format("|%n");
+        }
+        String over = gameOver() ? "over" : "not over";
+        out.format("] %d (max: %d) (game is %s) %n", score(), maxScore(), over);
+        return out.toString();
+    }
+
+    @Override
+    /** Returns whether two models are equal. */
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        } else if (getClass() != o.getClass()) {
+            return false;
+        } else {
+            return toString().equals(o.toString());
+        }
+    }
+
+    @Override
+    /** Returns hash code of Model’s string. */
+    public int hashCode() {
+        return toString().hashCode();
+    }
+}
---
+++ Pro0/src/main/java/game2048/Tile.java
@@ -0,0 +1,106 @@
+package game2048;
+
+import java.util.Objects;
+
+/** Represents the image of a numbered tile on a 2048 board.
+ *  @author P. N. Hilfinger.
+ */
+public class Tile {
+
+    /** A new tile with VALUE as its value at (ROW, COL).  This
+     *  constructor is private, so all tiles are created by the
+     *  factory methods create, move, and merge. */
+    private Tile(int value, int col, int row) {
+        this.value = value;
+        this.row = row;
+        this.col = col;
+        this.next = null;
+    }
+
+    /** Return my current row. */
+    public int row() {
+        return row;
+    }
+
+    /** Return my current column. */
+    public int col() {
+        return col;
+    }
+
+    /** Return the value supplied to my constructor. */
+    public int value() {
+        return value;
+    }
+
+    /** Return my next state.  Before I am moved or merged, I am my
+     *  own successor. */
+    public Tile next() {
+        return next == null ? this : next;
+    }
+
+    /** Return a new tile at (ROW, COL) with value VALUE. */
+    public static Tile create(int value, int col, int row) {
+        return new Tile(value, col, row);
+    }
+
+    /** Return the result of moving me to (COL, ROW). */
+    public Tile move(int col, int row) {
+        Tile result = new Tile(value, col, row);
+        next = result;
+        return result;
+    }
+
+    /** Return the result of merging OTHERTILE with me after moving to
+     *  (COL, ROW). */
+    public Tile merge(int col, int row, Tile otherTile) {
+        assert value == otherTile.value();
+        next = otherTile.next = new Tile(2 * value, col, row);
+        return next;
+    }
+
+    /** Return the distance in rows or columns between me and my successor
+     *  tile (0 if I have no successor). */
+    public int distToNext() {
+        if (next == null) {
+            return 0;
+        } else {
+            return Math.max(Math.abs(row - next.row()),
+                            Math.abs(col - next.col()));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d@(%d, %d)", value(), col(), row());
+    }
+
+    /** My value. */
+    private final int value;
+
+    /** My last position on the board. */
+    private final int row, col;
+
+    /** Successor tile: one I am moved to or merged with. */
+    private Tile next;
+
+    @Override
+    public boolean equals(Object obj) {
+        // 如果是同一个对象，直接返回true
+        if (obj == this) {
+            return true;
+        }
+        // 如果对象为空或者类型不匹配，返回false
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        // 强制转换为MyClass类型
+        Tile other = (Tile) obj;
+        // 判断两个对象的值是否相等
+        return row == other.row && col == other.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(col,row);
+    }
+}
